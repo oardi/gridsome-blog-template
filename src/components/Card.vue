b4e3964514d7be8cc3a815c86a2919d1766ea77b
Ardian Shala <ardian.sh@gmx.de>
2019-03-18 14:09:06 +0100

    Update Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,11 @@
 <template functional>
 	<div class="card shadow-sm">
 		<div class="card-body">
-			<h1 v-if="props.title" class="card-title">
-				{{ props.title }}
-			</h1>
+			<div v-if="props.title" class="card-title">
+				<div class="display-4">
+					{{ props.title }}
+				</div>
+			</div>
 			<div class="card-text">
 				<slot />
 			</div>
